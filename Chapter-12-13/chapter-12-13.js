fee25b773b0b7d95acde0833b23ec9dca718f256
Khalid-Parvaiz <khalidahmed.student0@gmail.com>
2025-08-22 13:24:31 +0500

    added folder

---
+++ Chapter-12-13/chapter-12-13.js
@@ -0,0 +1,34 @@
+// Question 01
+// var nmbr = +prompt("enter a No.")
+// if (nmbr >= 65 || nmbr <= 90){
+//     console.log("Your letter is Capital letter")
+// } else if (nmbr >= 97 || nmbr <= 122){
+//     console.log("Your letter is Small letter")
+// } else {
+//     console.log("Letter cannot found")
+// }
+
+// Question 02
+// var integer1 = +prompt("Enter a first integer")
+// var integer2 = +prompt("Enter a second integer")
+// if (integer1 == integer2){
+//     document.write(integer1+ " and " +integer2+ " are equal.")
+// } else if (integer1 > integer2){
+//     document.write(integer1+ " is largerthan " +integer2)
+// } else {
+//     document.write(integer1+ " is smallerthan " +integer2)
+// }
+
+// Question 03
+// var userNmbr = +prompt("Enter a Number.")
+// if (userNmbr > 0){
+//     document.write(userNmbr+ " is positive.")
+// } else if (userNmbr < 0){
+//     document.write(userNmbr+ " is negative.")
+// } else {
+//     document.write(userNmbr+ " is zero.")
+// }
+
+// Question 04
+var word = prompt("Write any word")
+    console.log(word.length[1])
---
+++ Chapter-14-16/chapter-14-16.js
@@ -0,0 +1,4 @@
+var arr = []
+var studentName = prompt("enter student name")
+arr.push(studentName)
+console.log(arr)
